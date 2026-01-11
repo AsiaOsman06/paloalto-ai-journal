3e013bffe78ca02b1e50f60d354d696d98bb30a4
Asia Osman <asiaosman06@gmail.com>
2026-01-11 11:54:09 -0600

    add journal entry and summary API endpoints

--- src/main.py
+++ src/main.py
@@ -5,3 +5,10 @@ app = FastAPI() # it use it to create new app
 @app.get("/") # it a path creater and defines a path
 def read_root():
     return {"FastAPI is working"}
+@app.post("/entry")
+def create_entry(text: str):
+    return add_entry(text)
+
+@app.get("/summary")
+def summary():
+    return get_summary()
